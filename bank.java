e673e812da28c65b4fe0fdccd4b26a3ac0b7dadc
Harsh Jain <harshjain.3125x@gmail.com>
2025-08-28 21:51:27 +0530

    made deposit new user and getconsid and createing table completely working

--- bank.java
+++ bank.java
@@ -5,12 +5,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import javax.swing.JOptionPane;
 
 public class bank {
-    public static int get_cons_id(int ph_no) {
-        final String DB_URL = "jdbc:mysql://127.0/0/1:3306/bank_backend";
+    public static int get_cons_id(long ph_no) {
+        final String DB_URL = "jdbc:mysql://127.0.0.1:3306/bank_backend";
         final String DB_USER = "root";
         final String DB_PASSWORD = "5202";
 
@@ -18,7 +19,7 @@ public class bank {
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
                 PreparedStatement preparedStatement = connection.prepareStatement(
                         "SELECT cons_id FROM all_cons WHERE mob_no=?")) {
-            preparedStatement.setInt(1, ph_no);
+            preparedStatement.setLong(1, ph_no);
             ResultSet rs = preparedStatement.executeQuery();
             if (rs.next()) {
                 id = rs.getInt("cons_id");
@@ -74,15 +75,48 @@ public class bank {
         return false;
     }
 
-    public static String deposit(int co_id, int amm, String mediu, int id) {
+    public static void deposit(int co_id) {
         // date
-        final String DB_URL = "jdbc:mysql://127.0/0/1:3306/bank_backend";
+        LocalDate curr=LocalDate.now();
+        String da=curr.toString();
+        final String DB_URL = "jdbc:mysql://127.0.0.1:3306/bank_backend";
         final String DB_USER = "root";
         final String DB_PASSWORD = "5202";
-        // the ammount will be updated inthe bank_contumer table and the one more row
-        // will be inserted in the user's table
+        int ammou=Integer.parseInt(JOptionPane.showInputDialog("Enter Ammount to Deposit"));
+        String medi=JOptionPane.showInputDialog("Enter Medium");
+                        // will be inserted in the user's table
+                    try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                         PreparedStatement preparedStatement = connection.prepareStatement(
+                "insert into c"+co_id+ "(Date,ammount,medium) values (?,?,?)")) {
+                    preparedStatement.setString(1,da);
+                     preparedStatement.setInt(2,ammou);
+                      preparedStatement.setString(3,medi);
+                      int rowsAffected = preparedStatement.executeUpdate();
+                      JOptionPane.showMessageDialog(null,"Deposited Succesfully"+rowsAffected);
 
-        return "Deposited Succesfully";
+                }
+        
+
+        catch(Exception e){
+            e.getStackTrace();
+            JOptionPane.showMessageDialog(null,"Error Found");
+        }
+        // the ammount will be updated inthe bank_contumer table and the one more row
+        try(Connection connection =DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        PreparedStatement pstmt=connection.prepareStatement("update  all_cons set balance=balance +? where cons_id=?")){
+            pstmt.setInt(1,ammou);
+            pstmt.setInt(2,co_id);
+            int rowsAffected=pstmt.executeUpdate();
+            JOptionPane.showMessageDialog(null,"rows affected"+rowsAffected);
+        }
+                    catch(SQLException e){
+                        JOptionPane.showMessageDialog(null,"Error Found");
+                        
+                    }
+
+        
+
+        
     }
 
     public static String withdraw(int co_id, int amm, String medium, int id) {
@@ -129,7 +163,33 @@ public class bank {
 
     }
 
+    public static void create_tab(int cid) {
+        final String DB_URL = "jdbc:mysql://127.0.0.1:3306/bank_backend";
+        final String DB_USER = "root";
+        final String DB_PASSWORD = "5202";
+        String sqlsta = "CREATE TABLE IF NOT EXISTS " + "C" + cid + " (" +
+                "    trans_no INT PRIMARY KEY AUTO_INCREMENT," +
+                "    Date DATE," +
+                "    ammount INT NOT NULL," +
+                "    medium VARCHAR(25) DEFAULT 'CASH'" +
+                ")";
+
+        try {
+            Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            java.sql.Statement stmt = con.createStatement();
+            stmt.executeUpdate(sqlsta);
+            JOptionPane.showMessageDialog(null, "Table Created");
+            stmt.close();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public static void new_user() {
+        LocalDate cuu=LocalDate.now();
+        String da=cuu.toString();
         // date
         final String DB_URL = "jdbc:mysql://127.0.0.1:3306/bank_backend";
         final String DB_USER = "root";
@@ -151,23 +211,28 @@ public class bank {
         // of constumer
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
                 PreparedStatement preparedStatement = connection.prepareStatement(
-                        "INSERT INTO all_cons(name,aadhar_no,gmail,balance,mob_no) VALUES( ?, ?, ?, ?,?);")) {
-           
+                        "INSERT INTO all_cons(name,aadhar_no,gmail,balance,mob_no,enroll_date) VALUES( ?, ?, ?, ?,?,?);")) {
+
             preparedStatement.setString(1, name);
             preparedStatement.setLong(2, adh_no);
             preparedStatement.setString(3, gmail);
             preparedStatement.setInt(4, min_bal);
             preparedStatement.setLong(5, mob_no);
+             preparedStatement.setString(6, da);
 
             int rowsAffected = preparedStatement.executeUpdate();
 
-            if (rowsAffected > 0){
+            if (rowsAffected > 0) {
                 JOptionPane.showMessageDialog(null, "User details added Sucessfully");
+                create_tab(get_cons_id(mob_no));
+                deposit(get_cons_id(mob_no));
+
+        
             }
 
         } catch (SQLException e) {
-             e.printStackTrace(); // print in console
-            
+            e.printStackTrace(); // print in console
+
             JOptionPane.showMessageDialog(null, "Error found");
 
         }
@@ -175,6 +240,7 @@ public class bank {
     }
 
     public static void main(String args[]) {
+        int cid_ = 0;
         final String DB_URL = "jdbc:mysql://127.0/0/1:3306/bank_backend";
         final String DB_USER = "root";
         final String DB_PASSWORD = "5202";
@@ -184,12 +250,12 @@ public class bank {
         check_looted();
 
         String name;
-        int mob_no;
+        long mob_no;
         name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Welcome to Bank's Backend Project");
-        mob_no = Integer.parseInt(JOptionPane.showInputDialog("Enter your registered phone no"));
+        mob_no = Long.parseLong(JOptionPane.showInputDialog("Enter your registered phone no"));
 
-        if (chk_cons_Exists(mob_no)) {
+        if (chk_cons_Exists(cid_)) {
             System.out.print(get_cons_id(mob_no));
             option = JOptionPane.showConfirmDialog(null, "Do you want to apply for Credit card",
                     "Apply for Credit Card", JOptionPane.YES_NO_OPTION);
@@ -205,7 +271,7 @@ public class bank {
                 // check if he/she is eligible for loan
             }
 
-            if (eligible_loan(mob_no)) {
+            if (eligible_loan(cid_)) {
                 JOptionPane.showMessageDialog(null, "Congratulations you are eligible for Pre Approved loan");
                 option = JOptionPane.showConfirmDialog(null, "Do you want to Apply", "Congratulations",
                         JOptionPane.YES_NO_OPTION);
@@ -253,7 +319,9 @@ public class bank {
         }
 
         else {
-            new_user();
+             new_user();
+            
+
         }
 
     }
